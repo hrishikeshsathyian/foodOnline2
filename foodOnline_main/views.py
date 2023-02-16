f8aeff1861b1e015e1996ae111e01922aa79bd76
hrishikeshsathyian <hrishikeshsathyiancoding@gmail.com>
2023-02-16 16:21:49 +0800

    Homepage setup, Postgres configuration and .env sample

--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello World")
+    context = {}
+    return render(request,'home.html',context)
