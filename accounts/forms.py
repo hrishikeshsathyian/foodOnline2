c460d22ee3e1b45ffebf9aaabb2d3f4fcd4a84ec
hrishikeshsathyian <hrishikeshsathyiancoding@gmail.com>
2023-03-10 15:30:14 +0800

    Vendor Profile Form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms 
-from .models import User
+from .models import User, UserProfile
+from .validators import allow_only_images_validator
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -18,5 +19,20 @@ class UserForm(forms.ModelForm):
                 "Password does not match"
             )
         
-class VendorForm(forms.ModelForm):
-    pass
+class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'start typing','required':'required'}))
+    profile_picture = forms.ImageField(widget=forms.FileInput(attrs={'class':'btn btn-info'}),validators=[allow_only_images_validator])
+    cover_photo= forms.ImageField(widget=forms.FileInput(attrs={'class':'btn btn-info'}))
+
+    #latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    #longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture','cover_photo','address','country','state','city','zip_code','latitude','longitude']
+
+    def __init__(self,*args,**kwargs):
+        super(UserProfileForm,self).__init__(*args,**kwargs)
+        for field in self.fields:
+            if field == 'latitude' or field == 'longitude':
+                self.fields[field].widget.attrs['readonly'] = 'readonly'
